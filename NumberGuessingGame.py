b7a819b398c4bfcb5386e796c8231d9a103354b8
hoimat <32225328+hoimat@users.noreply.github.com>
2017-09-23 20:40:59 +0300

    Update NumberGuessingGame.py

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -4,15 +4,16 @@ def Guessing():
     TheNumber = random.randint(1, 100)
     TotalGuesses = 1
     Guess = int(input('Your guess is: '))
-    while Guess != TheNumber:
+    
+	while Guess != TheNumber:
         TotalGuesses += 1
         if Guess < TheNumber:
             Guess = int(input("It's higher, guess again: "))
         elif Guess > TheNumber:
             Guess = int(input("It's lower, guess again: "))
-
     if Guess is TheNumber:
         print("That's right, you win! You guessed", TotalGuesses, 'times')
+
 
 Start = input('Do you want to play a number guessing game? y/n: ')
 
